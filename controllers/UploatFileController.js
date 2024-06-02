d866194bc15bb33acf1a63d5e0a266017deeb2e0
Gaston Herrlein <herrleingaston@gmail.com>
2024-06-02 23:15:02 +0200

    feat: se agrega redimencion de imagen

--- controllers/UploatFileController.js
+++ controllers/UploatFileController.js
@@ -1,13 +1,15 @@
 const multer = require("multer");
 
+const resizeImg = require("../lib/resizeImg");
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/images");
   },
   filename: function (req, file, cb) {
-    console.log(file);
-    console.log(req.body);
-    cb(null, Date.now() + "-" + file.originalname);
+    const name = Date.now() + "-" + file.originalname;
+    cb(null, name);
+    resizeImg(name);
   },
 });
 
@@ -16,5 +18,5 @@ const upload = multer({ storage: storage });
 exports.upload = upload.single("photo");
 
 exports.uploadFile = (req, res) => {
-  res.send({ data: "enviar un archivo" });
+  res.send({ data: "Imagen recibida" });
 };
