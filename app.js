187e2d1665aab4f15fdec9d2f6d068c737074757
Gaston Herrlein <herrleingaston@gmail.com>
2024-06-02 05:16:31 +0200

    fix: se agregan los controladores para las rutas

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const logger = require("morgan");
+
 const indexRouter = require("./routes/index");
 const articleRouter = require("./routes/api/article");
+const authRouter = require("./routes/api/auth");
+const authJWT = require("./lib/authMiddleware");
 
 var app = express();
 
@@ -15,13 +19,28 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
+// app.use(
+//   session({
+//     name: "WalaPop-session",
+//     secret: "sd7869f6789sfd8796sda87fsd978f768sdf8679s",
+//     saveUninitialized: true,
+//     resave: false,
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24, // 1d - expiración de la sesión por inactividad
+//     },
+//     store: MongoStore.create({
+//       mongoUrl: process.env.MONGODB_URL,
+//     }),
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/api/articles", articleRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/articles", authJWT, articleRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
